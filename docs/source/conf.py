97d721b03fccde580a89889fb584dbf9758dd77e
Miloslav Macko <miloslav.macko@rohde-schwarz.com>
2024-03-22 09:56:59 +0100

    corrected conf.py release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ author = 'Rohde & Schwarz'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '3.1.0'
 
 # -- General configuration ---------------------------------------------------
 
